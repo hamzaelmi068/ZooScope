3e5632fe769e775c7a9f6a74722952a6d7d5b79d
Hz <96888252+hamzaelmi068@users.noreply.github.com>
2024-02-24 21:56:03 -0500

    Update givenA1.h

--- givenA1.h
+++ givenA1.h
@@ -10,21 +10,6 @@
 #define NUM_TEST_DATA 20     // total number of samples / rows in the test dataset - you need this for task 9
 #define MAX_LENGTH_ANIMAL_NAME 50 // maximum length of an animal name in data.txt
 
-/*
-Student Name: Hamza Elmi
-Student ID: 1305966.
-Due Date: Feb 2 2024
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz
-I assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.
-*/
 
 struct Animal {
    
